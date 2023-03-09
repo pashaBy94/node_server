0f39e2b41815acd796f9304616a89c42d3af6394
Павел <120011959+pashaBy94@users.noreply.github.com>
2023-03-09 23:43:07 +0300

    app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const server = http.createServer(dataSubmit);
+function dataSubmit(request, respons){
+  console.log('Server listen');
+  let data = '';
+  request.on('data', el => data += el);
+  request.on('end', ()=>{
+    // let inMessag = JSON.parse(data);
+    console.log(data);
+    let outMessag = JSON.stringify({
+      title: 'Hello',
+      mes: `Your name: ${inMessag.name}, your age: ${inMessag.age}`
+    });
+    console.log(outMessag);
+    respons.statusCode = 200;
+    respons.setHeader('Content-Type', 'text/plain');
+    respons.end(outMessag);
+  });
+}
+server.listen(4000, 'localhost', (err)=>{
+  err?console.log(err):console.log('begin port 4000');
+});
+
