11dff7967a51de2599dadc145c059a891559c26c
pashaBy94 <pascom_by@mail.ru>
2023-03-09 23:24:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let form = document.forms[0];
+form.addEventListener = async function(ev){
+  ev.preventDefault();
+  let user = {
+    name: 'paha',
+    age: 27,
+};
+let response = await fetch('http://localhost:4000/', {
+  mode: "no-cors",
+  method: 'POST',
+headers:{
+  'Content-Type':'text/plain',
+},
+body: JSON.stringify(user),
+});
+console.log(response.status);
+let json = await response.json();
+console.log(json);
+}
